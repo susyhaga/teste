13f7267dcc61463306cb924d6506a7776536028b
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2023-11-22 10:59:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const nameElement = document.querySelector('#name');
-    const usernameElement = document.querySelector('#username');
-    const avatarElement = document.querySelector('#avatar');
-    const reposElement = document.querySelector('#repos');
-    const followersElement = document.querySelector('#followers');
-    const followingElement = document.querySelector('#following');
-    const linkElement = document.querySelector('#link');
+    const name = document.querySelector('#name');
+    const user = document.querySelector('#username');
+    const avatar = document.querySelector('#avatar');
+    const repos = document.querySelector('#repos');
+    const followers = document.querySelector('#followers');
+    const following = document.querySelector('#following');
+    const link = document.querySelector('#link');
     
     fetch('https://api.github.com/users/susyhaga')
         .then(function(element) {
             return element.json();
         })
         .then(function(json) {
-            nameElement.innerText = json.name;
-            usernameElement.innerText = json.login;
-            avatarElement.src = json.avatar_url;
-            followingElement.innerText = json.following;
-            followersElement.innerText = json.followers;
-            reposElement.innerText = json.public_repos;
-            linkElement.href = json.html_url;
+            name.innerText = json.name;
+            user.innerText = json.login;
+            avatar.src = json.avatar_url;
+            followers.innerText = json.followers;
+            following.innerText = json.following;
+            repos.innerText = json.public_repos;
+            link.href = json.html_url;
         })
         .catch (function(erro){
             alert('API incorreta. Confira sua API e tente novamente.');
