2fd6260089986dd4dff406ee6615af04fc3d7576
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2023-12-04 09:07:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //produto feminino com 18 reais de desconto
-function Produto_feminino(categoria, preco, tamanho){
-    this. categoria = categoria;
+function Produto(produto, preco, tamanho){
+    this. produto = produto;
     let _preco = preco;
     this.tamanho = tamanho;
 
@@ -17,18 +17,18 @@ _preco = novoPreco;
     }
 }
 
-const produto1 = new Produto_feminino('kimono', 180, ["pp", "p", "m", "g", "gg"]);
+const produto_feminino = new Produto('kimono', 180, ["pp", "p", "m", "g", "gg"]);
 
 
-console.log(Object.keys(produto1));
-console.log(Object.values(produto1));
+console.log(Object.keys(produto_feminino));
+console.log(Object.values(produto_feminino));
 
-console.log(`O ${produto1.categoria} feminino está com R$: ${produto1.calculaDesconto()} reais de desconto`); //15 reias de desconto
+console.log(`O ${produto_feminino.produto} feminino está com R$: ${produto_feminino.calculaDesconto()} reais de desconto`); //15 reias de desconto
 
 //produto masculino com 20 reais de desconto
-function Produto_masculino(categoria, preco, tamanho){
+function Produto_masculino(produto, preco, tamanho){
 
-    Produto_feminino.call(this, categoria, preco, tamanho);
+    Produto.call(this, produto, preco, tamanho);
 
     this.calculaDesconto = function(){
 
@@ -44,13 +44,13 @@ const produto2 = new Produto_masculino('camiseta',100,["pp", "p", "m", "g", "gg"
 console.log(Object.keys(produto2));
 console.log(Object.values(produto2));
 
-console.log(`A ${produto2.categoria} masculina está com R$: ${produto2.calculaDesconto()} reais de desconto`); //15 reias de desconto
+console.log(`A ${produto2.produto} masculina está com R$: ${produto2.calculaDesconto()} reais de desconto`); //15 reias de desconto
 
 //produto infantil com 100 reais de desconto
 
-function Produto_infantil(categoria, preco, tamanho){
+function Produto_infantil(produto, preco, tamanho){
 
-    Produto_feminino.call(this, categoria, preco, tamanho);
+    Produto.call(this, produto, preco, tamanho);
 
     this.calculaDesconto = function(){
 
@@ -61,10 +61,10 @@ function Produto_infantil(categoria, preco, tamanho){
     }
 }
 
-const produto3 = new Produto_infantil('Conjunto moleton',200,[4, 8, 12, 14, 16]);
+const produto3 = new Produto_infantil('conjunto moleton',200,[4, 8, 12, 14, 16]);
 
 
 console.log(Object.keys(produto3));
 console.log(Object.values(produto3));
 
-console.log(`A ${produto3.categoria} infantil está com R$: ${produto3.calculaDesconto()} reais de desconto`); //20 reias de desconto
+console.log(`A ${produto3.produto} infantil está com R$: ${produto3.calculaDesconto()} reais de desconto`); //20 reias de desconto
