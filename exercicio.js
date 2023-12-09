6aa6a90e93312600a3f05064977afaf24902da16
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2023-12-08 21:24:19 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,35 @@
+// • Cria um array de objetos, onde teremos como atributos do objeto: o nome e a nota de alunos;
+
+// • Criar uma função que irá retornar apenas os alunos que tiveram a nota maior ou igual à 6;
+
+// • Crie uma branch exercicio_es6 no repositório do curso, armazene o código nesta branch e nos envie o link através da plataforma. 
+
+class Alunos {
+    constructor(nomeDoAluno, notaDoAluno){
+        this.nome = nomeDoAluno;
+        this.nota = notaDoAluno;
+    }
+}
+
+const alunos = ['Gustavo', 'Julia', 'Paula', 'Wagner'];
+const notas = [ 10, 4, 5, 9];
+
+const nomesComNotas = alunos.map(function (nomeAtual) {
+  //objeto literal
+    return {
+    nome: nomeAtual,
+    nota: notas[
+        alunos.findIndex(function (item) {
+        //predicado
+        return item == nomeAtual;
+    })]
+
+    }
+});
+console.log(nomesComNotas);
+
+const apenasAlunosComNotaMaiorQue6 = nomesComNotas.filter(function (item) {
+    return item.nota >= 6;
+});
+
+console.log(apenasAlunosComNotaMaiorQue6);
