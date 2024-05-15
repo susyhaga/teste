a312bd6c5093f034ad3e1672a40b225575634089
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2024-05-15 09:53:30 -0300

    Add files via upload

---
+++ exercicio_ts.ts
@@ -0,0 +1,14 @@
+//MULTIPLICACAO
+function multiplica(a:number,b:number):number{
+    return a*b;
+}
+const result: number = multiplicacao(2, 9);
+console.log(resultado);
+
+
+//NOME
+function sauda(nome:string):string{
+    return "Ola!"+ nome;
+}
+const saudacaoconcat:string = sauda('Geleia');
+console.log(saudacaoconcat);
